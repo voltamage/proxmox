4b7d4e18f86327daea46bbff863d88735d01079d
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-05 04:31:46 -0700

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,6 @@
 alias crsync='sudo rsync --acls --archive --block-size=131072 --partial --progress --recursive --stats -vv'
 alias dark='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/dark)"'
-alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -A /dev/sda'
+alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -i /dev/sdb && sudo smartctl -A /dev/sda && sudo smartctl -A /dev/sdb'
 alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/notes/master/armbian/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
 alias light='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/light)"'
