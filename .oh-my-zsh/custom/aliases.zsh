13786d3f3967d8322478bf9358517985ae55d43d
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-05 03:55:13 -0700

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,6 +2,7 @@ alias crsync='sudo rsync --acls --archive --block-size=131072 --partial --progre
 alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -A /dev/sda'
 alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/notes/master/armbian/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
+alias light='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/light)"'
 alias mntdu='sudo ncdu /mnt'
 alias patch='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/patcher)"'
 alias sysdu='sudo ncdu / --exclude /mnt'
