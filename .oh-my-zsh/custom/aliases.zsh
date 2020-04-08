76d12c8f48b864752b664aa96c302ee94dc21f05
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-08 04:57:19 -0700

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,6 +2,7 @@ alias backport="dpkg -l | grep '~bpo'"
 alias crsync='sudo rsync --acls --archive --block-size=131072 --partial --progress --recursive --stats -vv'
 alias dark='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/dark)"'
 alias diskhealth='sudo smartctl -i /dev/sda && sudo smartctl -i /dev/sdb && sudo smartctl -A /dev/sda && sudo smartctl -A /dev/sdb'
+alias glusterv="sudo glusterfs --version | grep 'glusterfs '"
 alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/common/master/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
 alias light='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/proxmox/master/tmp/light)"'
